059c132cce973baf8713182b52ca64b7c09982d3
Valentin Podkamennyi <valentin@dtm.io>
2017-10-05 16:23:47 -0700

    Updated 'redirect' to komito.net

--- komito.js
+++ komito.js
@@ -1,10 +1,9 @@
 /* @license http://www.apache.org/licenses/LICENSE-2.0 */
 (function() {
-  /** @type {string} */ var src = 'https://komito.net/komito.js';
-  /** @type {Element} */ var script = document.createElement('SCRIPT');
-
-  script.src = src;
+  var script = document.createElement('SCRIPT');
+  script.src = 'https://komito.net/komito.js';
   document.body.appendChild(script);
 
-  console.log('Deprecated. Please use "' + src + '" instead.');
+  console.log('This site uses an outdated version of Komito Analytics script.\n' +
+              'Please visit https://komito.net/integration/ for more details.');
 })();
