37d29f4b06964f4ba6895e3327b41855bc4a5f07
Valentin Podkamennyi <valentin@dtm.io>
2017-10-09 21:34:02 -0700

    Fixed scroll event tracking.

--- src/komito/trackers/dom/scroll.js
+++ src/komito/trackers/dom/scroll.js
@@ -13,17 +13,19 @@ komito.trackers.dom.Scroll = function() {
    */
   function init_() {
     if (komito.config['trackScroll']) {
-      /** @type {Object} */ var map = {25: 0, 50: 0, 75: 0, 100: 0};
+      /** @type {!Object} */ var map = {25: 0, 50: 0, 75: 0, 100: 0};
       /** @type {Element} */ var root = dom.document.documentElement;
+      /** @type {Element} */ var body = dom.document.body;
       /** @type {number} */ var depth;
 
       dom.events.addEventListener(
           dom.context, dom.events.TYPE.SCROLL, function() {
-            /** @type {number} */ var percent =
-                (root.scrollTop + dom.document.body.scrollTop) /
-                (root.scrollHeight - root.clientHeight) * 100;
+            var screenHeight = root.clientHeight || dom.context.innerHeight;
+            var scrollHeight = root.scrollHeight || body.offsetHeight;
+            var scrollTop = root.scrollTop || body.scrollTop;
+            var percent = scrollTop / (scrollHeight - screenHeight) * 100;
             depth = ~~(percent / 25) * 25;
-            if (depth && !map[depth]) {
+            if (depth && depth in map && !map[depth]) {
               map[depth] = 1;
               komito.track(
                   komito.EVENT_ACTION_TYPE, 'scroll', 'depth', depth + '%');
