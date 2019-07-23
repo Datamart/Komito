6d6e497e255c2b4995806e3a4536197e2d06faf5
Valentin Podkamennyi <valentin@dtm.io>
2019-07-22 20:48:44 -0700

    Updated npm script.

--- build/npmjs.sh
+++ build/npmjs.sh
@@ -2,8 +2,11 @@
 
 readonly CWD=$(cd $(dirname $0); pwd)
 readonly NPM="$(which npm)"
+readonly TMP="${CWD}/komito"
+
+mkdir "${TMP}" && cd "${TMP}"
 
 "${NPM}" init --force
 "${NPM}" i komito-analytics
 
-rm -rf node_modules package-lock.json package.json
+rm -rf "${TMP}"
