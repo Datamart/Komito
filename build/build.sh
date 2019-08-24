ed9a2d865fb6a2ccb6c732752873628a9316886f
Valentin Podkamennyi <valentin@dtm.io>
2019-08-23 23:04:51 -0700

    Updated build.sh

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,10 @@
 # Guide: https://google.github.io/styleguide/shell.xml
 
 readonly CWD=$(cd $(dirname $0); pwd)
+readonly USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0"
+readonly HTTP_REFERER="https://wordpress.org/plugins/komito-analytics/"
+readonly PLUGIN_FILE="komito-analytics.zip"
+readonly PLUGIN_URL="https://downloads.wordpress.org/plugin/${PLUGIN_FILE}"
 
 #
 # Prints message.
@@ -14,6 +18,17 @@ function println() {
 }
 
 #
+# Downloads WordPress plugin.
+#
+function download() {
+  curl --user-agent "${USER_AGENT}" \
+       --referer "${HTTP_REFERER}" \
+       --cookie "wporg_locale=en_US" \
+       "${PLUGIN_URL}" > ${PLUGIN_FILE}
+  rm -rf ${PLUGIN_FILE}
+}
+
+#
 # The main function.
 #
 function main() {
@@ -22,6 +37,9 @@ function main() {
 
   println "[BUILD] Running compiler:"
   chmod +x "${CWD}/jsmin.sh" && "${CWD}/jsmin.sh"
+
+  println "[BUILD] Downloading WordPress plugin:"
+  download
 
   println "[BUILD] Done"
 }
